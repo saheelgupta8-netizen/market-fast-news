a63b824696f5ed44f7c95edc820bd34c7416ee01
saheelgupta8-netizen <saheelgupta8@gmail.com>
2026-07-11 13:05:54 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,37 @@
+KEYWORDS = [
+    # Solar
+    "vikram solar",
+    "waaree",
+    "premier energies",
+    "renewsys",
+    "websol",
+    "insolation energy",
+
+    # Defence
+    "bel",
+    "bharat electronics",
+    "bdl",
+    "bharat dynamics",
+    "hal",
+    "hindustan aeronautics",
+    "mazagon dock",
+    "cochin shipyard",
+    "paras defence",
+    "data patterns",
+
+    # Aerospace
+    "aequs",
+
+    # IPO
+    "ipo",
+    "grey market premium",
+    "gmp",
+    "subscription",
+    "listing"
+]
+
+RSS_FEEDS = [
+    "https://www.moneycontrol.com/rss/business.xml",
+    "https://www.moneycontrol.com/rss/markets.xml",
+    "https://www.cnbctv18.com/commonfeeds/v1/eng/rss/market/rss.xml"
+]
