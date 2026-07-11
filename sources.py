19f7274a7dddd94d83973921152f0af228bd5b68
saheelgupta8-netizen <saheelgupta8@gmail.com>
2026-07-11 13:21:56 +0530

    Create sources.py

---
+++ sources.py
@@ -0,0 +1,17 @@
+RSS_FEEDS = [
+    # Moneycontrol
+    "https://www.moneycontrol.com/rss/business.xml",
+    "https://www.moneycontrol.com/rss/markets.xml",
+
+    # CNBC TV18
+    "https://www.cnbctv18.com/commonfeeds/v1/eng/rss/market/rss.xml",
+
+    # LiveMint
+    "https://www.livemint.com/rss/markets",
+
+    # Business Standard
+    "https://www.business-standard.com/rss/markets-106.rss",
+
+    # Economic Times Markets
+    "https://economictimes.indiatimes.com/markets/rssfeeds/1977021501.cms"
+]
