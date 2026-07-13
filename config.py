e3c9dad1c591b84a61d757f4d2c0b24be9ff21a0
saheelgupta8-netizen <saheelgupta8@gmail.com>
2026-07-13 12:23:25 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,67 @@
+xKEYWORDS = [
+    # Solar
+    "vikram solar",
+    "waaree",
+    "premier energies",
+    "renewsys",
+    "websol",
+    "insolation energy",
+    "solar",
+    "solar cell",
+    "solar module",
+
+    # Defence
+    "bel",
+    "bharat electronics",
+    "bdl",
+    "bharat dynamics",
+    "hal",
+    "hindustan aeronautics",
+    "mazagon dock",
+    "cochin shipyard",
+    "paras defence",
+    "data patterns",
+    "defence",
+    "defense",
+    "missile",
+    "army",
+    "navy",
+    "air force",
+
+    # Aerospace
+    "aequs",
+
+    # IPO
+    "ipo",
+    "grey market premium",
+    "gmp",
+    "subscription",
+    "listing",
+    "anchor",
+
+    # Global Market
+    "iran",
+    "israel",
+    "oil",
+    "crude oil",
+    "gold",
+    "fed",
+    "federal reserve",
+    "china",
+    "tariff",
+
+    # Corporate
+    "order",
+    "contract",
+    "deal",
+    "agreement",
+    "approval",
+    "acquisition",
+    "merger",
+    "results",
+    "dividend",
+    "bonus",
+    "split",
+    "export",
+    "government"
+]
